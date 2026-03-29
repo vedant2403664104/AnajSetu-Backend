d50f8f3bea16c8f97bfd2d6414994ae061605fb5
atharvapawaskar <atharvapawaskar08@gmail.com>
2026-03-29 15:18:25 +0530

    completed changes in user.java

--- src/main/java/com/example/anajsetu/model/User.java
+++ src/main/java/com/example/anajsetu/model/User.java
@@ -22,7 +22,7 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private int id;
 
     @Column(nullable = false)
     private String name;
